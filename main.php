2886b9d198bfcbc3935e96b0448fd1e964cc3073
ketraking1 <kking34@student.gsu.edu>
2013-07-28 14:05:06 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,99 @@
+<html>
+  <head> 
+		<title> Trendy Women's Clothing, Juniors, Shoes & Dresses: Company Name </title>
+		<link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/main.css" />
+		<link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/main.css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	</head>
+
+	<body scroll="yes">
+		<h1 id="comName"> Company Name </h1>
+
+		<div class="topcorner"><a href="login.php">Login</a> / <a href="registration.php"</a>Register</div>
+		<div class="undertopcorner"><a href="shoppingcart.php">Shopping Cart</a></div>
+
+		<div class="middle">
+			<table border='1' align="center" width="100%">
+				<tr>
+					<th>Inventory!!!!</th>
+					<th>Images!!!</th>
+				</tr>
+			
+				<tr>
+					<td><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoes.php">Shoes</a></td>
+					<td><img src="http://codd.cs.gsu.edu/~c43711/Final_Project/images/chocolate_gift_shoes.jpg" height="100" width="100" /></td> 
+				</tr>
+		
+				<tr>
+					<td><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoes.php">Dresses</a></td>
+					<td><img src="http://codd.cs.gsu.edu/~c43711/Final_Project/images/dresses.jpg" height="100" width="100" /></td>
+				</tr>
+
+		
+				<tr>	
+					<td><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoes.php">Accessories</a></td>
+					<td><img src="http://codd.cs.gsu.edu/~c43711/Final_Project/images/accessories.jpg" height="100" width="100" /></td>
+				</tr>
+
+			</table>
+		</div>
+			<p>
+t
+t
+t
+t
+t
+t
+t
+t
+t
+t
+t
+t
+t
+t
+
+t
+t
+
+
+t
+t
+t
+
+
+
+			</p>
+
+
+		</div>
+		<div class="maincontainer">
+			<div id="container1">
+				<table align="center">
+					<tr><th>Customer Support!!</th></tr>
+					<tr><td><a href="contactpage.php" >Contact Us</a></td></tr>
+					<tr><td><a href="" >Shipping</a></td></tr>
+					<tr><td><a href="" >Returns</a></td></tr>
+					<tr><td><a href="aboutus.php" >About Us</a></td></tr>
+					<tr><td><a href="main.php" >Home</a></td></tr>
+				</table>
+			</div>
+
+
+			<div id="container2">
+				<table align="center">
+					<tr><th>Stores Near You!!</th></tr>
+					<tr><td>Clothes Lane Mall</td></tr>
+					<tr><td>International Blvd Outlet</td></tr>
+					<tr><td>Lakeshore Mall</td></tr>
+					<tr><td>Lenox Shopping Center</td></tr>
+				</table>
+			</div>
+		</div>
+
+
+
+	</body>
+
+</html>
