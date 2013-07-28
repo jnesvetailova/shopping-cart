53b4a3b5cf9dd98cbc9bb90f21aace4a82ed4348
ketraking1 <kking34@student.gsu.edu>
2013-07-28 14:07:05 -0400

    Create codeexec.php

---
+++ codeexec.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+	include('connection.php');
+	$fullname = $_POST['newuserReg'];
+	$username1 = $_POST['newuserUserName'];
+	$email = $_POST['newuserEmail'];
+	$password1 = $_POST['newuserPass'];
+
+	mysql_query("INSERT INTO usertbl_fp(name, username, email, password) VALUES('$fullname', '$username1', '$email', '$password1')");
+	header("location: index.php?remarks=sucess");
+	mysql_close($con);
+
+
+
+?>
