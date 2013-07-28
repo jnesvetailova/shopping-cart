e500f48f9ae995eb835381a3ac7577897f85ab28
ketraking1 <kking34@student.gsu.edu>
2013-07-28 14:06:17 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+		<link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/main.css" />
+	</head>
+
+	<body>
+		<h2 align="center"> Contact Us!!! </h2>
+
+		<header class="mainheader">
+			<img src="http://codd.cs.gsu.edu/~c43711/Final_Project/images/project2(2).png" alt="logo" />
+			<nav>
+				<ul>
+					<li class="currentpage"><a href="./index.php">Home</a></li>
+					<li><a href="./about.php">About</a></li>
+					<li><a href="./contact.php">Contact</a></li>
+					<li></li>
+				</ul>
+			
+			</nav>
+		</header>
+
+		<form action="" name="form1" method="POST">
+			<table align="center">
+				<tr><td>Enter Subject: </td><td><input type="text" name="Subject"</td></tr>
+				<tr><td>Enter Full Name: </td><td><input type="text" name="fullname"</td></tr>
+				<tr><td>Enter Email Address: </td><td><input type="text" name="email"</td></tr>
+				<tr><td>Enter Comment or Question: </td><td><textarea rows="8" cols="22"></textarea></td></tr>
+				<tr><td></td><td><input type="Submit" name="button2" value="Send!" /></td</tr>
+
+			</table>
+		</form>
+	</body>
+	
+</html>
+
