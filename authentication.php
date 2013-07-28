7a5dcbf72c95ed0b33d35b12617b474a628d7a72
ketraking1 <kking34@student.gsu.edu>
2013-07-28 14:07:52 -0400

    Create authentication.php

---
+++ authentication.php
@@ -0,0 +1,12 @@
+<?php
+  //start session
+	session_start();
+	
+	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == ' ')) {
+		header("location: login.php");
+		exit();
+
+	}
+	
+	
+?>
