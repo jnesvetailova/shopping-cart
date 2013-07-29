4b1bb1790707b9b88dce410aefdb7ff8ec5c5aff
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-29 16:02:00 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  	<link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/shop.css" />
+<title>About Us</title>
+	</head>
+
+	<body>
+		<div id="wrapper">
+	<div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+	
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	
+</div>
+	<div id="logo" class="container">
+		<h1>About Us!</h1>
+	</div>
+
+	<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/github_logo_zps0791944b.jpg" width="324" height="369" alt="" /></div>
+					<h2>Resources Used:</h2>
+					<p>We used GitHub to share and compare our code!</p>
+									</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/CircleLogo-WEB_zps50501b9e.gif" width="324" height="320" alt="" /></div>
+					<h2>created for Web Programming 4370 class</h2>
+					<p>at Georgia State University, summer 2013 semester by <b>Ketra King and Jenny Nesvetailova</b></p>
+									</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/mysql-logo_zpsf8c1b6c7.jpg" width="324" height="343" alt="" /></div>
+					<h2>How We Did It:</h2>
+					<p>We created an online store with the use of MySQL databases and PHP</p>
+					
+				</div>
+			</div>
+		</div>
+
+			</div>
+		</div>
+
+
+	</div>
+	
+	
+</div>
+
+	</body>
+
+</html>
