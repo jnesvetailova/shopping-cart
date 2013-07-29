c90d6c6339ab1d4b6af108866fdbc181507e80db
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-29 15:28:14 -0400

    Create accessories.php

---
+++ accessories.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Shop Accessories</title>
+<link rel="stylesheet" type="text/css" href="shop.css">
+</head>
+<body>
+
+<div id="wrapper">
+  <div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+	
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="logo" class="container">
+		<h1>Shop Accessories</h1>
+		<p>Here you can find a variety of dresses that we offer for sale in our shop</p>
+	</div>
+
+
+<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/purse_zps2d7aa966.jpg" width="324" height="370" alt="" /></div>
+					<h2>Big Purse</h2>
+					<p>Perfect for your everyday needs and everyday accessories!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=7" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/danglingearrings_zps2702145b.jpg" width="324" height="393" alt="" /></div>
+					<h2>Dangling Earrings</h2>
+					<p>Perfect for workplace environment or fun in the sun!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=9" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/studearrings_zps0264c1e4.jpg" width="324" height="393" alt="" /></div>
+					<h2>Stud Earrings</h2>
+					<p>Perfect for workplace environment or fun in the sun!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=10" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+
+<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/sunglasses_zps32c28dec.jpeg" width="324" height="350" alt="" /></div>
+					<h2>Sunglasses</h2>
+					<p>Perfect for your everyday activities in the sun!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=8" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+	
+	
+</div>
+
+</body>
+
+</html>
