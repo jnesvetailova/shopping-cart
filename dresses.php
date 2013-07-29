40644d65ea42875aa965e0fb0db2297dccd2ec0e
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-29 15:14:48 -0400

    Create dresses.php

---
+++ dresses.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Shop Dresses</title>
+<link rel="stylesheet" type="text/css" href="shop.css">
+</head>
+<body>
+
+<div id="wrapper">
+  <div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li class="current_page_item"><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+	
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="logo" class="container">
+		<h1><a href="#">Shop Dresses</a></h1>
+		<p>Here you can find a variety of dresses that we offer for sale in our shop</p>
+	</div>
+
+
+<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/businesscasdress_zps204fe353.jpg" width="324" height="478" alt="" /></div>
+					<h2>Business/Conservative Dress</h2>
+					<p>Perfect dress for job interviews or the work place environment!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=2" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/funmaxidress_zps2c6a0fe6.jpeg" width="324" height="500" alt="" /></div>
+					<h2>Fun Maxi Dress</h2>
+					<p>Perfect dress for fun day time activities!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=3" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/littleblackdress_zpsc27c2e77.jpg" width="324" height="500" alt="" /></div>
+					<h2>Little Black Dress</h2>
+					<p>Perfect dress for date night or a fun night on the town!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=1" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+</div>
+
+</body>
+
+</html>
