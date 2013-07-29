4171a3b2e219f7430b56931fdc2b08f85e22bd14
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-29 15:39:02 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  	<link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/shop.css" />
+	</head>
+
+	<body>
+		<div id="wrapper">
+	<div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+	
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="logo" class="container">
+		<h1>Contact Us!</h1>
+	</div>
+		<form action="" name="form1" method="POST">
+			<table align="center">
+				<tr><td>Enter Subject: </td><td><input type="text" name="Subject"</td></tr>
+				<tr><td>Enter Full Name: </td><td><input type="text" name="fullname"</td></tr>
+				<tr><td>Enter Email Address: </td><td><input type="text" name="email"</td></tr>
+				<tr><td>Enter Comment or Question: </td><td><textarea rows="8" cols="22"></textarea></td></tr>
+				<tr><td></td><td><input type="Submit" name="button2" class="button" value="Send!" /></td</tr>
+
+			</table>
+		</form>
+	</body>
+
+</html>
