3b7321afeb6f2a155e86094ccb3dd07e2d731651
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-29 15:01:25 -0400

    Create shoes.php

---
+++ shoes.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Shop Shoes</title>
+<link rel="stylesheet" type="text/css" href="shop.css">
+</head>
+<body>
+
+<div id="wrapper">
+  <div id="menu-wrapper">
+		<div id="menu" class="container">
+			<ul>
+				<li class="current_page_item"><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+	
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="logo" class="container">
+		<h1><a href="#">Shop Shoes</a></h1>
+		<p>Here you can find a variety of shoes that we offer for sale in our shop</p>
+	</div>
+
+
+<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/summersandals_zpsb8574a51.jpg" width="324" height="300" alt="" /></div>
+					<h2>Summer Sandals</h2>
+					<p>Perfect flat sandals for anytime of the day!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=6" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/funbrownwedge_zpsc1344d14.jpeg" width="324" height="300" alt="" /></div>
+					<h2>Fun Brown Wedges</h2>
+					<p>Perfect shoes for night or day activities!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=4" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="http://i52.photobucket.com/albums/g34/car_alarms/shoppingcart/nighttimeheel_zps9dab8fb3.jpg" width="324" height="300" alt="" /></div>
+					<h2>Night Heels </h2>
+					<p>Perfect heels for date night or fun nights on the town!</p>
+					<a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php?page=products&action=add&id=5" class="button">Add To Cart</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+</div>
+
+</body>
+
+</html>
