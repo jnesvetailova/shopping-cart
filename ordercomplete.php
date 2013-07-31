0b0277e31e88d22a79ef3f6c737baf1adf3c2ac5
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-30 23:28:12 -0400

    Create ordercomplete.php

---
+++ ordercomplete.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+
+
+
+        <head> 
+                <title>Order Complete</title>
+                <link rel="stylesheet" href="http://codd.cs.gsu.edu/~c43711/Final_Project/css/shop.css" />
+        <head>
+
+        <body>
+
+<div id="wrapper">
+  <div id="menu-wrapper">
+  	<div id="menu" class="container">
+			<ul>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/main.php">Home</a></li>
+
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/login.php">Login</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/registration.php">Register</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/shoppingcart.php">Shopping Cart</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/contact.php">Contact Us</a></li>
+				<li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/about.php">About Us</a></li>
+		  <li><a href="http://codd.cs.gsu.edu/~c43711/Final_Project/logout.php">Log Out</a></li>
+
+			</ul>
+		</div>
+	</div>
+
+
+
+<div id="logo" class="container">
+		<h1>Thank you for your order!</h1>
+	</div>
+
+<p align="center">It will be on its way to you shortly!</p>
+
+</body>
+</html>
